b8e14020180756ffbbdd18032773ce721c45d912
JRS80 <jason.steffen@gmail.com>
2022-06-24 17:19:54 -0500

    factory pattern exercise

---
+++ FactoryPattern/IStudent.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public interface IStudent
+    {
+        string StudentType { get; set; }
+        string Name { get; set; } 
+        int Age { get; set; } 
+        bool IsInState { get; set; }
+        bool IsGraded { get; set; } 
+        int StudentIDNumber { get; set; }
+
+        void SetStudentInfo();
+    }
+
+    public enum StudentTypes
+    {
+        Graduate,
+        Undergraduate,
+        NonDegreeSeeking
+    }
+
+
+    public static class StudentFactory
+    {
+        public static IStudent CreateStudent(StudentTypes studentType)
+        {
+            if(studentType == StudentTypes.Graduate) return new Graduate();
+            else if(studentType == StudentTypes.Undergraduate) return new Undergraduate();
+            else if(studentType == StudentTypes.NonDegreeSeeking) return new NonDegreeSeeking();
+            else return null;
+        }
+    }
+
+    public abstract class Student:IStudent
+    {
+
+        public string StudentType { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool IsInState { get; set; }
+        public bool IsGraded { get; set; }
+        public int StudentIDNumber { get; set; }
+
+        public abstract void SetStudentInfo();
+
+        //Need to add error handling if e.g. user enters a letter instead of numbers for age:
+        public virtual void RequestStudentInfo() {
+            Console.WriteLine("Please enter student information in order to create a new student.  All fields are required.");
+            Console.WriteLine("\tName: ");
+            this.Name = Console.ReadLine();
+            Console.WriteLine("\tAge: ");
+            this.Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\tDoes student qualify for in-state tuition?  [Enter 1 for yes or 0 for no.]");
+            char tmp = Console.ReadKey().KeyChar;
+            if(tmp == '1') this.IsInState = true;
+            else if(tmp == '0') this.IsInState = false;
+            else {
+                Console.WriteLine("Invalid input.  Student marked as out-of-state for now.");
+                this.IsInState = false;
+                Console.ReadKey(true);
+            }         
+        }
+
+        //Need to add error handling in case the generated id is already in the student list.  Just generate a new one.
+        public int GenerateNewID()
+        {
+            Random r = new Random();
+            int id = r.Next(1, 100000);
+            return id;
+        }
+    }
+
+    
+
+    public class Graduate : Student
+    {
+        public Graduate()
+        {
+            SetStudentInfo();
+        }
+        public override void SetStudentInfo()
+        {
+            IsGraded = true;
+            this.StudentType = "Graduate";
+            this.RequestStudentInfo();
+            this.StudentIDNumber = GenerateNewID();
+        }
+    }
+
+    public class Undergraduate: Student
+    {
+        public Undergraduate()
+        {
+            SetStudentInfo();
+        }
+        
+        public override void SetStudentInfo()
+        {
+            this.IsGraded = true;
+            this.StudentType = "Undergraduate";
+            RequestStudentInfo();
+            this.StudentIDNumber = GenerateNewID();
+        }
+    }
+
+    public class NonDegreeSeeking : Student
+    {
+        public NonDegreeSeeking()
+        {
+            SetStudentInfo();
+        }
+        public override void SetStudentInfo()
+        {
+            this.IsGraded = false;
+            this.StudentType = "Non-degree-seeking";
+            RequestStudentInfo();
+            this.StudentIDNumber = GenerateNewID();
+        }
+    }
+
+}
---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            bool userFinished = false;
+
+            var studentList = new Dictionary<int, string>();
+
+            while(!userFinished) {
+                Console.Clear();
+                Console.WriteLine("\n\nWelcome to the University Student database.  Select an option: ");
+                Console.WriteLine("\t1. Enter a new student.\n\t2. Show student list.\n\t3. Exit");
+
+                ConsoleKeyInfo key = Console.ReadKey(false);
+
+                switch(key.KeyChar) {
+                    case '3':
+                        userFinished = true;
+                        break;
+                    case '2':
+                        if(studentList == null) {
+                            Console.WriteLine("\nStudent List is empty.");
+                            break;
+                        }
+                        Console.WriteLine("\n\n------------------------------------------");
+                        Console.WriteLine("----Student Name----------Student ID-----");
+                        Console.WriteLine("------------------------------------------");
+                        foreach(var v in studentList) Console.WriteLine($"\t{v.Key}\t\t{v.Value}");
+                        Console.WriteLine("\n\nPress any key to return to main menu.");
+                        Console.ReadKey(true);
+                        break;
+                    case '1':
+                        Console.WriteLine("\n\nEnter type of student to create:\n\t1. Graduate\n\t2. Undergraduate\n\t3. Non-degree-seeking");
+                        char k = Console.ReadKey(false).KeyChar;
+                        StudentTypes st = k == '1' ? StudentTypes.Graduate : (k == '2' ? StudentTypes.Undergraduate : StudentTypes.NonDegreeSeeking);
+                        IStudent newStudent = StudentFactory.CreateStudent(st);
+                        if(newStudent == null) {
+                            Console.WriteLine("Error creating student.");
+                            Console.ReadKey(true);
+                        }
+                        else {
+                            Console.WriteLine($"\n{newStudent.StudentType} student created.");
+                            studentList.Add(newStudent.StudentIDNumber, newStudent.Name);
+                            Console.ReadKey(true);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("\nInvalid input.");
+                        Console.ReadKey(true);
+                        break;
+                }
+            }
+
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey(true);
+
+        }
+    }
+}
